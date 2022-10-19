a4aae86973bc2bc716dbad63db93b5173d5bae49
kjh7592 <sajh0405@gmail.com>
2022-10-19 12:52:45 +0900

    스캐너 입력받기

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -1,7 +1,5 @@
 package com.kor.java.ssg;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,75 +7,20 @@ public class Main {
 		System.out.println("===프로그램 시작===");
 
 		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("명령어)");
+		String command = sc.next();
+		System.out.printf("입력된 명령어 : %s\n", command);
 		
-		int lastArticleId = 0;
-
-		List<Article> articles = new ArrayList<>();
-
-		while (true) {
-			System.out.printf("명령어)");
-
-			String command = sc.nextLine();
-			
-			command = command.trim();
-
-			if (command.length() == 0) {
-				System.out.println("명령어를 입력해주세요");
-				continue;
-			} else if (command.equals("system exit")) {
-				break;
-			}
-
-			else if (command.equals("article list")) {
-				if (articles.size() == 0) {
-					System.out.println("게시물이 존재하지 않습니다");
-				} else {
-					for (Article article : articles) {
-
-						System.out.println("번호 : " + article.id);
-						System.out.println("제목 : " + article.title);
-						System.out.println("제목 : " + article.body);
-					}
-				}
-			}
-
-			else if (command.equals("article write")) {
-				int id = lastArticleId + 1;
-				lastArticleId = id;
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-
-				System.out.println("제목 : " + title);
-				System.out.println("내용 : " + body);
-
-				Article article = new Article(id, title, body);
-				articles.add(article);
-				
-				System.out.println(id + "번 글이 생성되었습니다");
-			}
-
-			else {
-				System.out.printf("%s(은)는 존재하지 않는 명령어입니다\n",command);
-				continue;
-			}
-		}
+//		String command = sc.nextLine();
+		
+		System.out.printf("명령어)");
+		int num = sc.nextInt();
+		System.out.printf("입력된 숫자 : %d\n", num);
+		
 
 		sc.close();
 
 		System.out.println("===프로그램 끝===");
-	}
-}
-
-class Article {
-	int id;
-	String title;
-	String body;
-	
-	public Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
 	}
 }
