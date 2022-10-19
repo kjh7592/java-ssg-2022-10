dee58ec2c8d86e4832700d08fbd8e51fba4ad061
kjh7592 <sajh0405@gmail.com>
2022-10-19 21:24:03 +0900

    게시물 작성 기능 구현중

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -8,15 +8,40 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.printf("명령어)");
-		String command = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", command);
+		int lastArticleId = 0;
 		
-//		String command = sc.nextLine();
+		while(true) {
+			System.out.printf("명령어)");
+			String command = sc.nextLine();
+			
+			command = command.trim();
+			
+			if(command.length() == 0) {
+				System.out.println("명령어를 입력해주세요.");
+				continue;
+			}
+			if(command.equals("system exit")) {
+				break;
+			}
+			else if(command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			}
+			else if(command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			}
+			
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
+			}
+		}
 		
-		System.out.printf("명령어)");
-		int num = sc.nextInt();
-		System.out.printf("입력된 숫자 : %d\n", num);
 		
 
 		sc.close();
