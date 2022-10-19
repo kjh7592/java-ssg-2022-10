3f515428ff3819dd84117ad24f3464bf15afd2eb
kjh7592 <sajh0405@gmail.com>
2022-10-19 11:16:23 +0900

    게시물 작성 기능 구현중

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -9,22 +9,26 @@ public class Main {
 		System.out.println("===프로그램 시작===");
 
 		Scanner sc = new Scanner(System.in);
+		
+		int lastArticleId = 0;
 
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.printf("명령어)");
 
-			String cmd = sc.nextLine();
+			String command = sc.nextLine();
+			
+			command = command.trim();
 
-			if (cmd.length() == 0) {
+			if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
-			} else if (cmd.equals("exit")) {
+			} else if (command.equals("system exit")) {
 				break;
 			}
 
-			else if (cmd.equals("article list")) {
+			else if (command.equals("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 존재하지 않습니다");
 				} else {
@@ -37,8 +41,9 @@ public class Main {
 				}
 			}
 
-			else if (cmd.equals("article write")) {
-				int id = articles.size();
+			else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -46,14 +51,15 @@ public class Main {
 
 				System.out.println("제목 : " + title);
 				System.out.println("내용 : " + body);
-				id++;
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
+				
+				System.out.println(id + "번 글이 생성되었습니다");
 			}
 
 			else {
-				System.out.println("존재하지 않는 명령어입니다");
+				System.out.printf("%s(은)는 존재하지 않는 명령어입니다\n",command);
 				continue;
 			}
 		}
