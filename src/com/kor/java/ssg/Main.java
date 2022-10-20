23dfc671ac194bc8cffad25e822afff92e734fb9
kjh7592 <sajh0405@gmail.com>
2022-10-20 13:24:46 +0900

    게시물 작성시 작성날짜도 저장

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -30,12 +30,13 @@ public class Main {
 			else if(command.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.getNowDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 				
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -75,7 +76,7 @@ public class Main {
 					continue;
 				}
 				System.out.println("번호 : " + foundArticle.id);
-				System.out.println("날짜 : " + "2022-12-12 12:12:12");
+				System.out.println("날짜 : " + foundArticle.regDate);
 				System.out.println("제목 : " + foundArticle.title);
 				System.out.println("내용 : " + foundArticle.body);
 			}
@@ -119,11 +120,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
